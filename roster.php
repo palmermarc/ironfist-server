18213468825647038ec4c70c1ef62315d7bea7e4
Marc Palmer <palmermarc@gmail.com>
2020-12-05 18:01:14 -0600

    Updated the Raider.io checks
    Updated the calls so that we only update characters who are member or higher and are max level.

--- roster.php
+++ roster.php
@@ -6,18 +6,8 @@ include_once 'core.php';
 
 $core = new Core;
 
-var_dump( $core->get_character_raider_io( 'us', 'agamaggan', 'palmer' ) );
-
-die();
-
-
-
-
-
 $access_token = $core->get_access_token();
 $guild = $core->get_guild_roster( $access_token );
-
-$fp = fopen( 'members.json', 'w');
 
 $roster = [];
 
@@ -34,8 +24,27 @@ if( !empty ( $guild->members ) ) :
 			'level' => $member->character->level,
 			'class' => $classes[$member->character->playable_class->id],
 			'race' => $races[$member->character->playable_race->id],
-			'rank' => $member->rank,
+			'guild_rank' => $member->rank,
+      'guild_rank_string' => $ranks[$member->rank]
 		];
+
+    if( $member_data['guild_rank'] <= 6 && $member_data['level'] == 60 ) {
+      echo "Pulling the Raider.io data for {$member_data['name']}\n";
+      $raider_data = $core->get_character_raider_io( 'us', $member->character->realm->slug, $member->character->name );
+
+      $member_data['race'] = $raider_data->race;
+      $member_data['class'] = $raider_data->class;
+      $member_data['active_spec_name'] = $raider_data->active_spec_name;
+      $member_data['active_spec_role'] = $raider_data->active_spec_role;
+      $member_data['gender'] = $raider_data->gender;
+      $member_data['faction'] = $raider_data->faction;
+      $member_data['achievement_points'] = $raider_data->achievement_points;
+      $member_data['honorable_kills'] = $raider_data->honorable_kills;
+      $member_data['thumbnail_url'] = $raider_data->thumbnail_url;
+      $member_data['last_crawled_at'] = $raider_data->last_crawled_at;
+      $member_data['profile_url'] = $raider_data->profile_url;
+      $member_data['profile_banner'] = $raider_data->profile_banner;
+    }
 
 		$fp = fopen( $member_file, 'w' );
 		fwrite( $fp, json_encode( $member_data ) );
